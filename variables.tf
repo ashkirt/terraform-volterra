9f920304899b7b0092804d8e31a5a4d5814cfb22
ashkirt <42281611+ashkirt@users.noreply.github.com>
2025-07-15 17:11:33 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,8 +2,13 @@ variable "VES_tenant_url" {
   description = "Tenant URL for Volterra provider"
   type        = string
 }
-variable "VES_p12_certificate_content" {
+variable "VES_P12_CONTENT" {
   description = "Base64 encoded content of the .p12 certificate file."
+  type        = string
+  sensitive   = true
+}
+variable "VES_P12_PASSWORD" {
+  description = "password"
   type        = string
   sensitive   = true
 }
