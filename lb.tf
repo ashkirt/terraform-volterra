ff4f3ae1310a6e4b31c32fa659a41708d3b21b3c
ashkirt <42281611+ashkirt@users.noreply.github.com>
2025-08-13 07:00:16 -0400

    Update lb.tf

--- lb.tf
+++ lb.tf
@@ -1,14 +1,14 @@
 //==========================================================================
 //Definition of the Origin
 //Start of the TF file
-resource "volterra_origin_pool" "asingla-test-pool" {
-  name                   = "asingla-test-pool"
+resource "volterra_origin_pool" "asingla-test-pool1" {
+  name                   = "asingla-test-pool1"
   //Name of the namespace where the origin pool must be deployed
   namespace              = "asingla"
   origin_servers {
 
     public_ip {
-      ip = "54.152.165.236"
+      ip = "54.152.165.237"
     }
 
     labels = {
@@ -24,8 +24,8 @@ resource "volterra_origin_pool" "asingla-test-pool" {
 //==========================================================================
 
 //Definition of the WAAP Policy
-resource "volterra_app_firewall" "asingla-waap-new" {
-  name      = "asingla-waap-new"
+resource "volterra_app_firewall" "asingla-waap-new1" {
+  name      = "asingla-waap-new1"
   namespace = "asingla"
 
   // One of the arguments from this list "allow_all_response_codes allowed_response_codes" must be set
